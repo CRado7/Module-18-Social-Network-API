658cb9915002a1fbfd2578e25cc30ff51e979fe5
Christopher <christopher.ferraro34@gmail.com>
2024-03-27 21:54:46 -0400

    Create user.js

---
+++ modals/user.js
@@ -0,0 +1,41 @@
+const Schema = require('mongoose').Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thoughts'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+},
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
+);
+
+// BEGIN: abpxx6d04wxr
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
