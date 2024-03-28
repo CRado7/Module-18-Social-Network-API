a8329ca7fb8363517c13861684a5f8aec4c7012f
Christopher <christopher.ferraro34@gmail.com>
2024-03-27 22:00:32 -0400

    Update thoughts.js

---
+++ models/thoughts.js
@@ -0,0 +1,39 @@
+const {Schema, model} = require('mongoose').Schema;
+
+const thoughtsSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Reactions'
+        }
+    ]
+},
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
+);
+
+thoughtsSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+})
+
+const Thoughts = model('Thoughts', thoughtsSchema);
+
+module.exports = Thoughts;
