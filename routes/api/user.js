51d9e477b8403eccf11720acf97d66070f2117ae
Christopher <christopher.ferraro34@gmail.com>
2024-03-27 21:40:37 -0400

    update

--- routes/api/user.js
+++ routes/api/user.js
@@ -21,4 +21,10 @@ router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser);
 // Set up POST and DELETE at /api/users/:userId/friends/:friendId
 router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
 
+// Set up GET one and DELETE at /api/users/:id
+router.route('/:id').get(getUserById).delete(deleteUser);
+
+// Set up GET all and POST at /api/users
+router.route('/').get(getAllUsers).post(createUser);
+
 module.exports = router;
